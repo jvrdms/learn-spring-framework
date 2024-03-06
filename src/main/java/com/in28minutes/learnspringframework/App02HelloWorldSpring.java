27b29070800894915485045f9a4fdf6fe25f26ef
jvrdms <jvrdms@gmail.com>
2024-03-06 20:19:20 +0100

    Step 10 - Implementing Auto Wiring in Spring Framework Java Configuration File

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -16,6 +16,9 @@ public class App02HelloWorldSpring {
         System.out.println(name);
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean("address2"));
+        // System.out.println(context.getBean(Address.class));
     }
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 // are automatically generated
 // Released in JDK 16
 
-record Person(String name, int age) {
+record Person(String name, int age, Address address) {
 };
 
 record Address(String street, String city, String state, String zip) {
@@ -30,11 +30,24 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("Raga", 25);
+        return new Person("Raga", 25, new Address("Point Street", "Springfield", "IL", "62701"));
     }
 
     @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name, age, address3);
+    }
+
+    @Bean(name = "address2")
     public Address address() {
         return new Address("1234 Main St", "Springfield", "IL", "62701");
+    }
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("Baker St", "London", "UK", "12345");
     }
 }
